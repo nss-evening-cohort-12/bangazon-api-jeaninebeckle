3b3922b80722391f4f9f69df6ca1d1efcf35e547
Jeanine Beckle <jeaninebeckle@gmail.com>
2021-02-19 19:45:58 -0600

    wip

---
+++ bangazonreports/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views.products import highproductprice_list
+
+urlpatterns = [
+    path('reports/highproductprice', highproductprice_list)
+]
---
+++ bangazonreports/views/products/__init__.py
@@ -0,0 +1,1 @@
+from .productsoveronethousand import highproductprice_list
